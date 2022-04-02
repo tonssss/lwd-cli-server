39c4c67f060038a2c91c03ee9d273a2147d7d6ca
tons <294343714@qq.com>
2022-04-02 16:50:47 +0800

    first

---
+++ app/utils/mongo.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Mongodb = require('@pick-star/cli-mongodb')
+const { mongodbUrl, mongodbDbName } = require('../../config/db')
+
+function mongo () {
+  return new Mongodb(mongodbUrl, mongodbDbName)
+}
+
+module.exports = mongo
+
---
+++ config/db.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/** MONGODB **/
+const mongodbUrl = 'mongodb://localhost:27017/lwd-cli'
+const mongodbDbName = 'lwd-cli'
+
+module.exports = {
+  mongodbUrl,
+  mongodbDbName
+}
